f7f5887db65803c1133bb85528110e7b46aaa9d7
daniel-mcelligott42 <danielmcelligott314@gmail.com>
2021-11-30 13:53:08 +0000

    Added first password check field making sure its 7 characters

---
+++ src/main/java/com/example/csfx/HelloApplication.java
@@ -0,0 +1,83 @@
+package com.example.csfx;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        GridPane grid = new GridPane();
+
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Text scenetitle = new Text("Enter username and password below");
+        scenetitle.setFont(Font.font("Verdana", FontWeight.NORMAL, 15));
+        grid.add(scenetitle, 0, 0, 2, 1);
+
+        Label userName = new Label("Username:");
+        grid.add(userName, 0, 1);
+
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
+
+        Label pw = new Label("Password:");
+        grid.add(pw, 0, 2);
+
+        PasswordField pwBox = new PasswordField();
+        grid.add(pwBox, 1, 2);
+
+        Button btn = new Button("Sign in");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btn);
+        grid.add(hbBtn, 1, 4);
+
+        final Text actiontarget = new Text();
+        grid.add(actiontarget, 1, 6);
+
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent e) {
+                String pw=pwBox.getText();
+                if (pw.length()<7 ){
+                    actiontarget.setFill(Color.FIREBRICK);
+                    actiontarget.setText("Weak password");
+                }
+                else {
+                    actiontarget.setFill(Color.FIREBRICK);
+                    actiontarget.setText("Welcome");
+                }
+            }
+        });
+
+        Scene scene = new Scene(grid, 400, 400);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
